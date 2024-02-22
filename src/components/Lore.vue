40f029e007f0c7de3b90fa7c2e09a285d91e5f03
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2024-02-22 20:59:19 +0100

    Creacion de componente Lore, reestructuracion de codigo y carpeta de estilos para componentes

---
+++ src/components/Lore.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div>
+        <h1>Lore</h1>
+        <p>Here is some lore about Elden Ring</p>
+        
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+
+
+</script>
+
+<style scoped>
+
+</style>
